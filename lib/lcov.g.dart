f0b8890879df182345a96264f7e19ddf1325ea2f
Cédric Belin <cedric@belin.io>
2020-01-03 18:18:06 +0100

    Updated the generated files

--- lib/lcov.g.dart
+++ lib/lcov.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'lcov.dart';
+part of lcov;
 
 // **************************************************************************
 // JsonSerializableGenerator
